727741bdf2eb576ab42ec45415c1f8fcfe9d4559
sivalaxman7 <sivalaxman.mylapilli@cogoport.com>
2022-08-05 21:23:56 +0530

    save

--- my-app/src/Components/AllTabs/SecondTab.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SecondTab = () => {
-  return (
-    <div>SecondTab</div>
-  )
-}
-
-export default SecondTab
--- my-app/src/Components/AllTabs/ThirdTab.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Thirdtab = () => {
-  return (
-    <div>Thirdtab</div>
-  )
-}
-
-export default Thirdtab
